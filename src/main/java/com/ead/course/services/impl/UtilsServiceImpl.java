5245877f7d814fe0a8a7de03684e030d2e8fce6d
vinizuppa <vcordeiro12@gmail.com>
2022-06-04 12:50:53 -0300

    Insert properties dev

--- src/main/java/com/ead/course/services/impl/UtilsServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/UtilsServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Service
 public class UtilsServiceImpl implements UtilsService {
 
-    @Value("${ead.api.url.authuser}")//Capta a URL definida no application.yaml
+    @Value("${ead.api.url.authuser}")//Capta a URL definida no application-dev.yaml
     String REQUEST_URL_AUTHUSER;
 
     public String createUrlGetAllUsersByCourse(UUID courseId, Pageable pageable){
